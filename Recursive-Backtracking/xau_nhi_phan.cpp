4e2da07982e1f4e3c71264d4f4babd6a9b7aae57
hasonsk <127868734+hasonsk@users.noreply.github.com>
2023-04-06 16:29:16 +0700

    Create xau_nhi_phan.cpp

---
+++ Recursive-Backtracking/xau_nhi_phan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#define MAX 10
+int N;
+int x[MAX];// bieu dien loi phuong an cua bai toan
+
+bool check(int v, int k){
+    return true;
+}
+void solution(){
+    for(int i = 1; i <= N; i++) cout << x[i] << " "; cout << endl;
+}
+void TRY(int k){
+    for(int v = 0; v <= 1; v++){
+        if(check(v,k)){
+            x[k] = v;
+            if(k == N) solution();
+            else TRY(k+1);
+        }
+    }
+}
+int main(){
+    N = 3;
+    TRY(1);// bat dau thu gia tri cho x[1]
+}
