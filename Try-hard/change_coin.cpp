0de787e68f55d7f079b6f50db7116fec2ea83180
hasonsk <127868734+hasonsk@users.noreply.github.com>
2023-06-30 22:20:14 +0700

    Create change_coin.cpp greedy

---
+++ Try-hard/change_coin.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int coin[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+int res;
+
+void solve(int n){
+    res = 0;
+    int i = 0; 
+    while(n != 0) {
+        if(coin[i] > n) { i++;}
+        else {n-=coin[i]; res++;}
+    }
+    cout << res;
+}
+
+int main() {
+    cin >> n;
+    solve(n);
+}
