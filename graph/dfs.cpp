75c161774e58d730d20df2080e92cee1da4b858d
hasonsk <127868734+hasonsk@users.noreply.github.com>
2023-07-29 09:33:23 +0700

    Update dfs.cpp

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -5,6 +5,8 @@ int const MAX = 100;
 int N, M;
 vector<int> Adj[MAX];
 vector<int> bVisited(MAX, false);
+vector<int> mark[MAX];
+int tplt; // thanh phan lien thong
 
 void input(){
     cin >> N >> M;
@@ -21,16 +23,28 @@ void dfs(int u) {
         return ;
     }
     bVisited[u] = true;
-    // cout << u << " ";
+    mark[tplt].push_back(u);
     for(int i = 0; i < Adj[u].size(); i++) {
         int v = Adj[u][i];
         dfs(v);
     }
 }
 
+
 int main() {
     input();
+    tplt = 0;
     for(int i = 1; i<=N; i++) {
-        if(!bVisited[i]) dfs(i);
+        if(!bVisited[i]){
+            tplt++;
+            dfs(i);
+        }
+    }
+    // tim so thanh phan lien thong
+    for(int i = 1; i<=tplt; i++) {
+        cout << "tplt #" << i << " : ";
+        for (int j = 0; j<mark[i].size(); j++)
+            cout << mark[i][j] << " ";
+        cout << endl;
     }
 }
